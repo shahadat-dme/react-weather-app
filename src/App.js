bcff82da8c3c64d84b1341559ea4e774a9bb1054
Shahadat <shahadat2cp17@gmail.com>
2022-01-23 19:41:20 +0600

    m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
+// import axios from 'axios'
 
 function App() {
 
-  const url =`https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=fe9b929bcbcb8a424b70a047929730b7`
-  
+  // const url =`https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=fe9b929bcbcb8a424b70a047929730b7`
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dhaka</p>
+          </div>
+          <div className="temp">
+            <h1>60*F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bootom">
+          <div className="feels">
+            <p>65*F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            12 MPH
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
