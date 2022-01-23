b802975a66321b15bd6fb3a28a6f260978772c8a
Shahadat <shahadat2cp17@gmail.com>
2022-01-23 18:42:17 +0600

    weather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+
+  const url =`https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=fe9b929bcbcb8a424b70a047929730b7`
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
